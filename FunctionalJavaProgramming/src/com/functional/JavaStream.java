33a40e80595ea6be256097d8e106fc4d6090933e
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-07-02 18:39:34 +0530

    Lambda Stream

---
+++ FunctionalJavaProgramming/src/com/functional/JavaStream.java
@@ -0,0 +1,56 @@
+package com.functional;
+
+import java.util.List;
+
+public class JavaStream {
+
+	public static void main(String[] args) {
+
+		// ListValuePrinter(List.of(10, 20, 25, 259, 251, 35, 52, 15, 2165, 31,77,89));
+		CourseValuePrinter(List.of("Spring Boot", "Spring MVC", "Spring Microservices", "Spring Rest", "Java", "Python", "MongoDB"));
+
+	}
+
+	/*public static void ListValuePrinter(List<Integer> numbers) {
+
+		numbers.stream().forEach(System.out::println);
+
+	}*/
+	
+	/*public static boolean isEven(int number) {
+		return number%2==0?true:false;
+	}
+
+	// using filter inside stream
+	public static void ListValuePrinter(List<Integer> numbers) {
+
+		numbers
+		.stream()
+		.filter(JavaStream::isEven)
+		.forEach(System.out::println);
+
+	}*/
+	
+	// Lambda expression
+	public static void ListValuePrinter(List<Integer> numbers) {
+
+		numbers
+		.stream()
+		.filter(n -> n%2==0?true:false)
+		.forEach(System.out::println);
+
+	}
+	
+	/*
+	 * 1. find the courses containing word 'Spring'
+	 * 2. find course name has 10 letters
+	 * */
+	private static void CourseValuePrinter(List<String> course) {
+
+		course
+		.stream()
+		.filter(c-> c.contains("Spring"))
+		.filter(c -> c.length()>10)
+		.forEach(System.out::println);
+	}
+}
